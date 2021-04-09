f32d3c05d693dd330a15b2db6ed3151131c22391
Manav Misra <manavm@visionwebsoft.com>
2021-04-09 09:11:35 -0500

    test: :white_check_mark: Test for mode switch
    Light/dark

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,12 @@
-import React from 'react';
 import { screen } from '@testing-library/react';
-import { render } from './test-utils';
+import React from 'react';
 import App from './App';
+import { render } from './test-utils';
 
-test('renders learn react link', () => {
+test('renders dark mode toggle button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn chakra/i);
+  const linkElement = screen.getByRole('button', {
+    ariaLabel: /^Switch to/,
+  });
   expect(linkElement).toBeInTheDocument();
 });
